132d769069b3cf542508fa367d4098d2ca3efb7b
Zhicheng Zhang <zzc-tongji@users.noreply.github.com>
2019-01-29 12:19:30 -0500

    question & answer

--- index.js
+++ index.js
@@ -2,16 +2,31 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-
 app.use(cors());
+
+// get
+
+const getList = [
+  '/help',
+  '/question/0100',
+  '/question/0200',
+  '/answer/0110',
+  '/answer/0120',
+  '/answer/0210',
+  '/answer/0220',
+];
+
+getList.forEach((value) => {
+  app.get(value, function (req, res) {
+    res.json(require(`./get${value}`));
+  });
+});
 
 app.get('/', function (req, res) {
   res.send('frontend-mock');
 });
 
-app.get('/help', function (req, res) {
-  res.json(require('./get/help'));
-});
+// server
 
 const server = app.listen(4000, function () {
   console.log('frontend-mock: port %s', server.address().port);
