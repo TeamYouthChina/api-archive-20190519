672e18ea71970bd57e3724fac6ef53276276fe23
gardness <gardness@live.com>
2019-02-16 17:51:27 -0500

    commit

--- get/applicants/1/applications.js
+++ get/applicants/1/applications.js
@@ -4,11 +4,26 @@ module.exports = {
   "content": [
     {
       "id": 0,
-      "companyId": 0,
-      "positionId": 0,
+      "position": {
+        "id": 0,
+        "name": "name",
+        "organization": {
+          "id": 0,
+          "name": "name",
+          "avatarUrl": "https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png",
+          "location": "location",
+          "website": "website",
+          "note": "note",
+          "nation": "nation"
+        },
+        "location": 0,
+        "type": "full-time",
+        "deadLine": "deadLine"
+      },
       "status": "status"
     }
   ],
+
   "status": {
     "code": 2000,
     "reason": "reason"
