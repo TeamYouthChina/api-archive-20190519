4cc216391b2d450a75f73f6188b8dba786fea381
gardness <gardness@live.com>
2019-02-19 16:32:33 -0500

    Modify /job/1.js

--- get/job/1.js
+++ get/job/1.js
@@ -7,10 +7,20 @@ module.exports = {
     "organization": {
       "id": 0,
       "name": "name",
-      "avatarUrl": "https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png"
+      "avatarUrl": "https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png",
+      "location": {
+        "nation_code": "nation_code",
+        "location_code": "location_code"
+      },
+      "website": "website",
+      "note": "note",
+      "nation": "nation"
     },
-    "location": 0,
-    "type": "full-time",
+    "location": {
+      "nation_code": "nation_code",
+      "location_code": "location_code"
+    },
+    "type": "type",
     "deadLine": "deadLine"
   },
   "status": {
