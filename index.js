6eb5b1c7790ea5f374d657d37efe5b47e2f79b29
gardness <gardness@live.com>
2019-02-27 15:32:41 -0500

    Update the rest of the API

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const getList = [
   '/answer/0210',
   '/answer/0210',
   '/answers/1',
+  '/answers/1/comments',
   '/applicants',
   '/applicants/1',
   '/applicants/1/applications',
@@ -22,6 +23,7 @@ const getList = [
   '/applicants/1/extracurriculars',
   '/applicants/1/projects',
   '/articles/1',
+  '/articles/1/comments',
   '/companies/1',
   '/disconvery/articles',
   '/discovery/companies',
@@ -29,6 +31,7 @@ const getList = [
   '/discovery/questions',
   '/discovery/users',
   '/editorials/1',
+  '/editorials/1/comments',
   '/help',
   '/home/new',
   '/home/hot',
@@ -43,12 +46,14 @@ const getList = [
   '/questions/1',
   '/questions/1/answers',
   '/users/1/attentions',
-  '/videos/1'
+  '/videos/1',
+  '/videos/1/comments'
 ];
 
 // post
 
 const postList = [
+  '/answers/1/comments',
   '/applicants',
   '/applicants/1/certificates',
   '/applicants/1/contacts',
@@ -56,15 +61,26 @@ const postList = [
   '/applicants/1/experiences',
   '/applicants/1/extracurriculars',
   '/applicants/1/projects',
+  '/applicants/resgister',
+  '/articles',
+  '/articles/1/comments',
+  '/companies',
+  '/editorials',
+  '/editorials/1/comments',
   '/jobs/1/apply',
   '/jobs/search',
+  '/login',
   '/questions',
-  '/questions/1/answers'
+  '/questions/1/answers',
+  '/videos',
+  '/videos/1/comments'
 ];
 
 // put
 
 const putList = [
+  '/answers/1',
+  '/answers/1/upvote',
   '/applicants/1',
   '/applicants/1/certificates/1',
   '/applicants/1/contacts',
@@ -72,26 +88,39 @@ const putList = [
   '/applicants/1/experiences/1',
   '/applicants/1/extracurriculars/1',
   '/applicants/1/projects/1',
+  '/articles/1',
+  '/articles/1/attention',
+  '/companies/1',
+  '/companies/1/attention',
+  '/editorials/1',
+  '/editorials/1/upvote',
+  '/editorials/1/attention',
   '/jobs/1/attention',
   '/questions/1',
   '/questions/1/attention',
   '/questions/1/invite',
-  '/questions/1/invite/1'
+  '/questions/1/invite/1',
+  '/videos/1/attention',
+  '/videos/1/upvote'
 ];
 
 // delete
 
 const deleteList = [
+  '/answers/1',
   '/applicants/1/certificates/1',
   '/applicants/1/educations/1',
   '/applicants/1/experiences/1',
   '/applicants/1/extracurriculars/1',
   '/applicants/1/projects/1',
+  '/articles/1',
   '/articles/attentions/1',
   '/companies/attentions/1',
+  '/editorials/1',
   '/jobs/collections/1',
   '/questions/1',
-  '/questions/attentions/1'
+  '/questions/attentions/1',
+  '/videos/1'
 ];
 
 
