b9b4337ae98631666ed2e6f31f819f59c729459c
gardness <gardness@live.com>
2019-02-27 21:52:29 -0500

    commit

--- get/home/new/index.js
+++ get/home/new/index.js
@@ -5,22 +5,221 @@ module.exports = {
     "jobList": [
       {
         "id": 0,
-        "name": "name",
+        "name": "运营测试工程师",
         "organization": {
           "id": 0,
-          "name": "name",
+          "name": "腾讯",
           "avatarUrl": "https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png",
-          "location": "location",
-          "website": "website",
+          "location": "北京",
+          "website": "https://www.qq.com/?fromdefault",
           "note": "note",
-          "nation": "nation"
+          "nation": "中国"
         },
-        "location": "location",
+        "location": "北京",
         "type": "full-time",
-        "deadLine": "deadLine",
-        "job_description": "job_description",
-        "job_duty": "job_duty"
+        "deadLine": "2020年10月2号",
+        "job_description": "有良好的沟通协调能力及团队协作能力，工作认真负责，规范性强，能按时保质完成工作任务；能给保证每周至少工作3天。",
+        "job_duty": "根据需求设计测试用例，编写测试程序以及测试脚本"
+      },
+      {
+        "id": 1,
+        "name": "软件开发工程师",
+        "organization": {
+          "id": 0,
+          "name": "百度",
+          "avatarUrl": "https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png",
+          "location": "北京",
+          "website": "http://www.baidu.com/",
+          "note": "note",
+          "nation": "中国"
+        },
+        "location": "北京",
+        "type": "full-time",
+        "deadLine": "2019年9月1号",
+        "job_description": "熟练使用VC++、C#或Java编程；熟悉Oracle/SQL Server等数据库开发；熟悉Web系统开发；",
+        "job_duty": "根据要求进行软件概要设计、详细设计、编码、单元测试工作及说明文档的编写"
+      },
+      {
+        "id": 2,
+        "name": "软件开发工程师",
+        "organization": {
+          "id": 0,
+          "name": "阿里巴巴",
+          "avatarUrl": "https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png",
+          "location": "浙江",
+          "website": "https://www.alibaba.com/",
+          "note": "note",
+          "nation": "中国"
+        },
+        "location": "浙江",
+        "type": "full-time",
+        "deadLine": "2022年3月1号",
+        "job_description": "熟练使用VC++、C#或Java编程；熟悉Oracle/SQL Server等数据库开发；熟悉Web系统开发；",
+        "job_duty": "根据要求进行软件概要设计、详细设计、编码、单元测试工作及说明文档的编写"
+      },
+      {
+        "id": 3,
+        "name": "软件开发工程师",
+        "organization": {
+          "id": 0,
+          "name": "阿里巴巴",
+          "avatarUrl": "https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png",
+          "location": "浙江",
+          "website": "https://www.alibaba.com/",
+          "note": "note",
+          "nation": "中国"
+        },
+        "location": "浙江",
+        "type": "full-time",
+        "deadLine": "2022年3月1号",
+        "job_description": "熟练使用VC++、C#或Java编程；熟悉Oracle/SQL Server等数据库开发；熟悉Web系统开发；",
+        "job_duty": "根据要求进行软件概要设计、详细设计、编码、单元测试工作及说明文档的编写"
+      },
+      {
+        "id": 4,
+        "name": "投资实习生",
+        "organization": {
+          "id": 0,
+          "name": "蚂蚁金服",
+          "avatarUrl": "https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png",
+          "location": "浙江",
+          "website": "https://www.antfin.com/",
+          "note": "note",
+          "nation": "中国"
+        },
+        "location": "浙江",
+        "type": "intern",
+        "deadLine": "2022年3月1号",
+        "job_description": "1、收集行业信息进行有效整理；2、长期跟踪行业信息并完成相关研究报告；",
+        "job_duty": "立志于将私募基金行业作为未来职业生涯规划，对投资项目具有浓厚的兴趣和热情；"
+      },
+      {
+        "id": 5,
+        "name": "php开发工程师",
+        "organization": {
+          "id": 0,
+          "name": "杭州阅云科技有限公司",
+          "avatarUrl": "https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png",
+          "location": "浙江",
+          "website": "https://www.yueyun.com/",
+          "note": "note",
+          "nation": "中国"
+        },
+        "location": "浙江",
+        "type": "full-time",
+        "deadLine": "2020年5月1号",
+        "job_description": "按照项目计划，遵循公司软件开发流程，按时提交高质量代码，完成开发任务",
+        "job_duty": "熟悉Linux，MySQL，熟悉B/S系统前后端相关开发技术"
+      },
+      {
+        "id": 6,
+        "name": "php开发工程师",
+        "organization": {
+          "id": 0,
+          "name": "杭州阅云科技有限公司",
+          "avatarUrl": "https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png",
+          "location": "浙江",
+          "website": "https://www.yueyun.com/",
+          "note": "note",
+          "nation": "中国"
+        },
+        "location": "浙江",
+        "type": "intern",
+        "deadLine": "2020年5月1号",
+        "job_description": "按照项目计划，遵循公司软件开发流程，按时提交高质量代码，完成开发任务",
+        "job_duty": "熟悉Linux，MySQL，熟悉B/S系统前后端相关开发技术"
+      },
+      {
+        "id": 7,
+        "name": "php开发工程师",
+        "organization": {
+          "id": 0,
+          "name": "杭州阅云科技有限公司",
+          "avatarUrl": "https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png",
+          "location": "浙江",
+          "website": "https://www.yueyun.com/",
+          "note": "note",
+          "nation": "中国"
+        },
+        "location": "浙江",
+        "type": "intern",
+        "deadLine": "2020年5月1号",
+        "job_description": "按照项目计划，遵循公司软件开发流程，按时提交高质量代码，完成开发任务",
+        "job_duty": "熟悉Linux，MySQL，熟悉B/S系统前后端相关开发技术"
+      },
+      {
+        "id": 8,
+        "name": "php开发工程师",
+        "organization": {
+          "id": 0,
+          "name": "杭州阅云科技有限公司",
+          "avatarUrl": "https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png",
+          "location": "浙江",
+          "website": "https://www.yueyun.com/",
+          "note": "note",
+          "nation": "中国"
+        },
+        "location": "浙江",
+        "type": "intern",
+        "deadLine": "2020年5月1号",
+        "job_description": "按照项目计划，遵循公司软件开发流程，按时提交高质量代码，完成开发任务",
+        "job_duty": "熟悉Linux，MySQL，熟悉B/S系统前后端相关开发技术"
+      },
+      {
+        "id": 9,
+        "name": "php开发工程师",
+        "organization": {
+          "id": 0,
+          "name": "杭州阅云科技有限公司",
+          "avatarUrl": "https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png",
+          "location": "浙江",
+          "website": "https://www.yueyun.com/",
+          "note": "note",
+          "nation": "中国"
+        },
+        "location": "浙江",
+        "type": "intern",
+        "deadLine": "2020年5月1号",
+        "job_description": "按照项目计划，遵循公司软件开发流程，按时提交高质量代码，完成开发任务",
+        "job_duty": "熟悉Linux，MySQL，熟悉B/S系统前后端相关开发技术"
+      },
+      {
+        "id": 10,
+        "name": "php开发工程师",
+        "organization": {
+          "id": 0,
+          "name": "杭州阅云科技有限公司",
+          "avatarUrl": "https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png",
+          "location": "浙江",
+          "website": "https://www.yueyun.com/",
+          "note": "note",
+          "nation": "中国"
+        },
+        "location": "浙江",
+        "type": "intern",
+        "deadLine": "2020年5月1号",
+        "job_description": "按照项目计划，遵循公司软件开发流程，按时提交高质量代码，完成开发任务",
+        "job_duty": "熟悉Linux，MySQL，熟悉B/S系统前后端相关开发技术"
+      },
+      {
+        "id": 11,
+        "name": "php开发工程师",
+        "organization": {
+          "id": 0,
+          "name": "杭州阅云科技有限公司",
+          "avatarUrl": "https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png",
+          "location": "浙江",
+          "website": "https://www.yueyun.com/",
+          "note": "note",
+          "nation": "中国"
+        },
+        "location": "浙江",
+        "type": "intern",
+        "deadLine": "2020年5月1号",
+        "job_description": "按照项目计划，遵循公司软件开发流程，按时提交高质量代码，完成开发任务",
+        "job_duty": "熟悉Linux，MySQL，熟悉B/S系统前后端相关开发技术"
       }
+      
     ]
   },
   "status": {
