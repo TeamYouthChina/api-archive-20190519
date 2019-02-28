b77107752209adbd66c350422617960cb5b5f670
gardness <gardness@live.com>
2019-02-28 16:13:21 -0500

    commit

--- get/companies/1/index.js
+++ get/companies/1/index.js
@@ -3,12 +3,12 @@
 module.exports = {
   "content": {
     "id": 0,
-    "name": "name",
+    "name": "腾讯",
     "avatarUrl": "https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png",
-    "location": "location",
-    "website": "website",
-    "note": "note",
-    "nation": "nation"
+    "location": "深圳",
+    "website": "https://www.qq.com/?fromdefault",
+    "note": "腾讯公司成立于1998年11月，是目前中国最大的互联网综合服务提供商之一，也是中国服务用户最多的互联网企业之一。成立15年多以来，腾讯一直秉承一切以用户价值为依归的经营理念，始终处于稳健、高速发展的状态。2004年6月16日，腾讯公司在香港联交所主板公开上市。",
+    "nation": "中国"
   },
   "status": {
     "code": 2000,
