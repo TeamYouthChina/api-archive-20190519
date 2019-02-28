4ff5f2e9c5989fdbd1d571490f67d0c62ea0e9fc
Zhicheng Zhang <zzc-tongji@users.noreply.github.com>
2019-02-28 16:53:49 -0500

    commit

--- get/discovery/companies/index.js
+++ get/discovery/companies/index.js
@@ -11,6 +11,42 @@ module.exports = {
         "website": "website",
         "note": "note",
         "nation": "nation"
+      },
+      {
+        "id": 0,
+        "name": "name",
+        "avatarUrl": "https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png",
+        "location": "location",
+        "website": "website",
+        "note": "note",
+        "nation": "nation"
+      },
+      {
+        "id": 0,
+        "name": "name",
+        "avatarUrl": "https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png",
+        "location": "location",
+        "website": "website",
+        "note": "note",
+        "nation": "nation"
+      },
+      {
+        "id": 0,
+        "name": "name",
+        "avatarUrl": "https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png",
+        "location": "location",
+        "website": "website",
+        "note": "note",
+        "nation": "nation"
+      },
+      {
+        "id": 0,
+        "name": "name",
+        "avatarUrl": "https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png",
+        "location": "location",
+        "website": "website",
+        "note": "note",
+        "nation": "nation"
       }
     ]
   },
