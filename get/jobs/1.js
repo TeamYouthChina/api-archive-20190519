fe7c5119a36fe669606da48ec9c050100364e085
gardness <gardness@live.com>
2019-02-28 14:48:23 -0500

    commit

--- get/jobs/1.js
+++ get/jobs/1.js
@@ -3,21 +3,21 @@
 module.exports = {
   "content": {
     "id": 0,
-    "name": "name",
+    "name": "测试实习生",
     "organization": {
       "id": 0,
-      "name": "name",
+      "name": "腾讯",
       "avatarUrl": "https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png",
-      "location": "location",
-      "website": "website",
+      "location": "北京",
+      "website": "https://www.qq.com/?fromdefault",
       "note": "note",
-      "nation": "nation"
+      "nation": "中国"
     },
-    "location": "location",
-    "type": "full-time",
-    "deadLine": "deadLine",
-    "job_description": "job_description",
-    "job_duty": "job_duty"
+    "location": "北京",
+    "type": "intern",
+    "deadLine": "2020年1月1号",
+    "job_description": "有嵌入式开发调试经验者优先，会使用交叉编译，串口调试助手等；",
+    "job_duty": "根据需求设计测试用例，编写测试程序以及测试脚本；"
   },
   "status": {
     "code": 2000,
